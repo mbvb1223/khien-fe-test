1546457a8545d021b83db4bc0a991d990d74dcbf
khien.pham <phamkhien@hotmail.com>
2019-05-05 22:50:54 +0700

    STORY-2: display the pointer for title, reformat code

--- src/app/core/interceptor/fakebackend.interceptor.ts
+++ src/app/core/interceptor/fakebackend.interceptor.ts
@@ -4,56 +4,56 @@ import { Observable, of, throwError } from 'rxjs';
 import { delay, mergeMap } from 'rxjs/operators';
 
 const users: Array<any> = [{
-	id: '1',
-	username: 'demo',
-	password: 'demo',
-	firstName: 'Pham',
-	lastName: 'Khien'
+  id: '1',
+  username: 'demo',
+  password: 'demo',
+  firstName: 'Pham',
+  lastName: 'Khien'
 }];
 
 @Injectable()
 export class FakeBackendInterceptor implements HttpInterceptor {
-	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		const {url, method, body} = request;
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const {url, method, body} = request;
 
-		return of(null)
-			.pipe(mergeMap(handleRoute))
-			.pipe(delay(500));
+    return of(null)
+      .pipe(mergeMap(handleRoute))
+      .pipe(delay(500));
 
-		function handleRoute() {
-			switch (true) {
-				case url.endsWith('/users/authenticate') && method === 'POST':
-					return authenticate();
-				default:
-					// pass through any requests not handled above
-					return next.handle(request);
-			}
-		}
+    function handleRoute() {
+      switch (true) {
+        case url.endsWith('/users/authenticate') && method === 'POST':
+          return authenticate();
+        default:
+          // pass through any requests not handled above
+          return next.handle(request);
+      }
+    }
 
-		function authenticate() {
-			const {username, password} = body;
-			const user = users.find(x => x.username === username && x.password === password);
-			console.log(user, username, password);
-			if (!user) {
-				return error('Username or password is incorrect');
-			}
+    function authenticate() {
+      const {username, password} = body;
+      const user = users.find(x => x.username === username && x.password === password);
+      console.log(user, username, password);
+      if (!user) {
+        return error('Username or password is incorrect');
+      }
 
-			return ok({
-				id: user.id,
-				username: user.username,
-				firstName: user.firstName,
-				lastName: user.lastName,
-				token: 'fake-jwt-token'
-			});
-		}
+      return ok({
+        id: user.id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        token: 'fake-jwt-token'
+      });
+    }
 
-		function ok(bodyData?) {
-			return of(new HttpResponse({status: 200, body: bodyData}));
-		}
+    function ok(bodyData?) {
+      return of(new HttpResponse({status: 200, body: bodyData}));
+    }
 
-		function error(message) {
-			return throwError({error: {message}});
-		}
-	}
+    function error(message) {
+      return throwError({error: {message}});
+    }
+  }
 }
 
