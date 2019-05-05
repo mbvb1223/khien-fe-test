b645a6dde32d063a40af6793bc5cbf32e7f5c768
khien.pham <phamkhien@hotmail.com>
2019-05-05 16:39:50 +0700

    STORY-1: add material, lodash

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import {MaterialConfigurationModule} from './material.module';
+
+
+@NgModule({
+    declarations: [
+    ],
+    imports: [
+        MaterialConfigurationModule
+    ],
+    exports: [
+    ]
+})
+export class SharedModule {
+}
