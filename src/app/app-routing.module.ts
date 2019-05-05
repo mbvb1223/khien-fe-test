550209579efc876c1b2f588bef5ea3933c1ba4f5
khien.pham <phamkhien@hotmail.com>
2019-05-05 16:51:08 +0700

    STORY-1: add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './core/component/login/login.component';
+import { PostListComponent } from './route/post/post-list/post-list.component';
+import { PostAddComponent } from './route/post/post-add/post-add.component';
+import { PostEditComponent } from './route/post/post-edit/post-edit.component';
+
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'post/list',
+    component: PostListComponent
+  },
+  {
+    path: 'post/add',
+    component: PostAddComponent
+  },
+  {
+    path: 'post/edit/:id',
+    component: PostEditComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MaterialConfigurationModule} from './share/material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { LoginComponent } from './core/component/login/login.component';
+import { PostListComponent } from './route/post/post-list/post-list.component';
+import { PostItemComponent } from './route/post/post-item/post-item.component';
+import { PostAddComponent } from './route/post/post-add/post-add.component';
+import { PostEditComponent } from './route/post/post-edit/post-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PostListComponent,
+    PostItemComponent,
+    PostAddComponent,
+    PostEditComponent
   ],
   imports: [
     BrowserModule,
